8826e434716d1e7ffe48c175661ffe8b73702c33
Lucas Matias <lucasmatiasnicacioxavier@gmail.com>
2024-08-30 17:39:32 -0300

    refactoring the crawler

--- src/RpaChallengeImpacta.API/Program.cs
+++ src/RpaChallengeImpacta.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using RpaChallengeImpacta.Domain.Models;
 using RpaChallengerImpacta.DataAccess.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,9 +19,22 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapPost("/api/data", () =>
+app.MapPost("/api/data", async (DatabaseContext context, List<Proxy> proxies) =>
 {
+    if (proxies == null || proxies.Count == 0)
+        return Results.BadRequest("No proxies provided.");
 
+    try
+    {
+        await context.AddRangeAsync(proxies);
+        await context.SaveChangesAsync();
+
+        return Results.Created("/api/data", proxies);
+    }
+    catch (Exception ex)
+    {
+        return Results.StatusCode(500);
+    }
 })
 .WithOpenApi();
 
---
+++ src/RpaChallengeImpacta.Crawler/UseCases/GetProxyDataFromProxyScape.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using RpaChallengeImpacta.Crawler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.Http.Json;
+
+namespace RpaChallengeImpacta.Crawler.UseCases
+{
+    internal static class GetProxyDataFromProxyScape
+    {
+        private static readonly string _siteUrl = "https://pt-br.proxyscrape.com/lista-de-procuradores-gratuitos";
+        public static async Task Run()
+        {
+            using var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl(_siteUrl);
+            
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            var jsonButton = wait.Until(driver =>
+                driver.FindElement(By.XPath("//button[small[text()='json']]"))
+            );
+
+            ScrollToElement(driver, jsonButton);
+
+            jsonButton.Click();
+
+            var downloadButton = wait.Until(driver =>
+                driver.FindElement(By.XPath("//button[small[text()='Download']]")));
+            downloadButton.Click();
+
+            await Task.Delay(3000);
+
+            var fileData = GetFileData();
+        }
+
+        #region Private Methods
+        private static List<ProxyDto> GetFileData()
+        {
+            try
+            {
+                var downloadDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
+
+                var directoryInfo = new DirectoryInfo(downloadDir);
+
+                var downloadedFile = directoryInfo
+                    .GetFiles()
+                    .Where(file => file.Name.Contains("proxies"))
+                    .OrderByDescending(x => x.LastWriteTime)
+                    .FirstOrDefault();
+
+                var fileContentString = File.ReadAllText(downloadedFile.FullName);
+
+                JsonElement proxiesElement = JsonDocument.Parse(fileContentString).RootElement.GetProperty("proxies");
+
+                var fileContentDto = JsonSerializer.Deserialize<List<ProxyDto>>(proxiesElement.GetRawText());
+
+                return fileContentDto;
+            }
+            catch(Exception ex)
+            {
+                return [];
+            }
+        }
+
+        private static void ScrollToElement(IWebDriver driver, IWebElement webElement)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", webElement);
+        }
+        #endregion
+    }
+}
